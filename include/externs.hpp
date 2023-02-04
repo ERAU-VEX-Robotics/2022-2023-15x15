d05b093e10addbc00daa76a30202e1fa9b0116d1
Nikhil Dave <nikhilrdave2003@gmail.com>
2023-02-03 18:18:54 -0700

    Added Conveyor to externs

--- include/externs.hpp
+++ include/externs.hpp
@@ -1,12 +1,15 @@
 #ifndef EXTERNS_HPP
 #define EXTERNS_HPP
 
+#include "Conveyor.hpp"
 #include "Drivetrain.hpp"
 #include "Flywheel.hpp"
 #include "Intake.hpp"
 
+
 extern Drivetrain drive;
 extern Intake intake;
 extern Flywheel flywheel;
+extern Conveyor conveyor;
 
 #endif /* externs.hpp */
