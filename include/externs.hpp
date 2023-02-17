d66ab494ead673992d961f5c59ef7ccf122faa85
Nikhil Dave <nikhilrdave2003@gmail.com>
2023-02-16 18:24:53 -0700

    Integrated Indexer code

--- include/externs.hpp
+++ include/externs.hpp
@@ -1,16 +1,16 @@
 #ifndef EXTERNS_HPP
 #define EXTERNS_HPP
 
-#include "Conveyor.hpp"
 #include "Drivetrain.hpp"
 #include "Flywheel.hpp"
+#include "Indexer.hpp"
 #include "Intake.hpp"
 #include "Roller.hpp"
 
 extern Drivetrain drive;
 extern Intake intake;
 extern Flywheel flywheel;
-extern Conveyor conveyor;
+extern Indexer indexer;
 extern Roller roller;
 
 #endif /* externs.hpp */
