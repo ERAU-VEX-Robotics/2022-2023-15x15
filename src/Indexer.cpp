8bcb29f8089c5733d4bf22101c0ac0422cab1d0f
Nikhil Dave <nikhilrdave2003@gmail.com>
2023-02-16 18:53:05 -0700

    Switched indexer motor to red cartridge

--- src/Indexer.cpp
+++ src/Indexer.cpp
@@ -1,17 +1,19 @@
 #include "Indexer.hpp"
 #include "pros/misc.h"
+#include "pros/motors.h"
 
 Indexer::Indexer(std::initializer_list<int> ports,
                  std::initializer_list<bool> revs)
     : motors(ports, revs) {
     motors.set_encoder_units(pros::E_MOTOR_ENCODER_DEGREES);
+    motors.set_gearing(pros::E_MOTOR_GEAR_RED);
 }
 
 void Indexer::set_rotation(int degrees_to_rotate) {
     this->degrees_to_rotate = degrees_to_rotate;
 }
 
-void Indexer::punch_disk() { motors.move_relative(degrees_to_rotate, 200); }
+void Indexer::punch_disk() { motors.move_relative(degrees_to_rotate, 100); }
 
 void Indexer::driver(pros::controller_id_e_t controller,
                      pros::controller_digital_e_t fire_btn) {
