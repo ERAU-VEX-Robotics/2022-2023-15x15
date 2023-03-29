ac7c80edc229d94d410b0a809a92414b0ae27938
Nikhil Dave <nikhilrdave2003@gmail.com>
2023-03-28 18:46:10 -0700

    Added test program for drive PID tuning

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -18,42 +18,26 @@ void autonomous() {
     drive.init_pid_task();
     drive.move_straight(0);
 
-    // Move to the roller and score it
-    drive.move_straight(-20);
-    drive.wait_until_settled();
-    drive.turn_angle(90);
-    drive.wait_until_settled();
-
-    drive.move_straight(-7);
-    drive.wait_until_settled();
-
-    roller.counterclockwise(120);
-
-    // Fire preloads into high goal
-    drive.move_straight(4);
-    drive.wait_until_settled();
-    drive.turn_angle(20);
-    drive.wait_until_settled();
-
-    indexer.punch_disk();
-    intake.in();
-    pros::delay(2000);
-    intake.stop();
-    indexer.punch_disk();
-    pros::delay(200);
-    flywheel.pause_task();
-
-    drive.turn_angle(-60);
-    drive.wait_until_settled();
-    drive.move_straight(60);
-    intake.in();
-    flywheel.resume_task();
+    drive.move_straight(24);
     drive.wait_until_settled();
 
     drive.turn_angle(90);
     drive.wait_until_settled();
-    indexer.punch_disk();
-    indexer.punch_disk();
+
+    drive.move_straight(-2);
+    drive.wait_until_settled();
+
+    drive.turn_angle(-90);
+    drive.wait_until_settled();
+
+    drive.move_straight(-12);
+    drive.wait_until_settled();
+
+    drive.turn_angle(45);
+    drive.wait_until_settled();
+
+    drive.turn_angle(45);
+    drive.wait_until_settled();
 
     switch (auton_id) {
     case test:
@@ -67,6 +51,44 @@ void autonomous() {
     case match_best:
         break;
     case match_real:
+
+        // Move to the roller and score it
+        drive.move_straight(-20);
+        drive.wait_until_settled();
+        drive.turn_angle(90);
+        drive.wait_until_settled();
+
+        drive.move_straight(-7);
+        drive.wait_until_settled();
+
+        roller.counterclockwise(120);
+
+        // Fire preloads into high goal
+        drive.move_straight(4);
+        drive.wait_until_settled();
+        drive.turn_angle(20);
+        drive.wait_until_settled();
+
+        indexer.punch_disk();
+        intake.in();
+        pros::delay(2000);
+        intake.stop();
+        indexer.punch_disk();
+        pros::delay(200);
+        flywheel.pause_task();
+
+        drive.turn_angle(-60);
+        drive.wait_until_settled();
+        drive.move_straight(60);
+        intake.in();
+        flywheel.resume_task();
+        drive.wait_until_settled();
+
+        drive.turn_angle(90);
+        drive.wait_until_settled();
+        indexer.punch_disk();
+        indexer.punch_disk();
+
         break;
     case none:
         break;
