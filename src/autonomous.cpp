83905c5c9be52e89f6f7debfe50dc4031679dfa9
Nikhil Dave <nikhilrdave2003@gmail.com>
2023-04-10 18:40:38 -0700

    Added Drivetrain PID tuning programs to auton

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -14,28 +14,43 @@
 void autonomous() {
     flywheel.resume_task();
     drive.init_pid_task();
-    drive.move_straight(0);
 
-    drive.move_straight(24);
-    drive.wait_until_settled();
-
-    drive.turn_angle(90);
-    drive.wait_until_settled();
-
-    drive.move_straight(-2);
-    drive.wait_until_settled();
-
-    drive.turn_angle(-90);
+    // Drive PID tuning programs
+    // Straight
+    /*
+    drive.move_straight(48);
     drive.wait_until_settled();
 
     drive.move_straight(-12);
     drive.wait_until_settled();
 
-    drive.turn_angle(45);
+    drive.move_straight(6);
     drive.wait_until_settled();
 
-    drive.turn_angle(45);
+    drive.move_straight(-24);
     drive.wait_until_settled();
+    */
+
+    // Turn
+    /*
+    drive.turn_angle(180);
+    drive.wait_until_settled();
+
+    drive.turn_angle(-150);
+    drive.wait_until_settled();
+
+    drive.turn_angle(120);
+    drive.wait_until_settled();
+
+    drive.turn_angle(-90);
+    drive.wait_until_settled();
+
+    drive.turn_angle(60);
+    drive.wait_until_settled();
+
+    drive.turn_angle(-30);
+    drive.wait_until_settled();
+    */
 
     switch (auton_id) {
     case test:
@@ -49,14 +64,13 @@ void autonomous() {
     case match_best:
         break;
     case match_real:
-
         // Move to the roller and score it
-        drive.move_straight(-20);
+        drive.move_straight(-12);
         drive.wait_until_settled();
         drive.turn_angle(90);
         drive.wait_until_settled();
 
-        drive.move_straight(-7);
+        drive.move_straight(-6);
         drive.wait_until_settled();
 
         roller.counterclockwise(120);
@@ -64,20 +78,18 @@ void autonomous() {
         // Fire preloads into high goal
         drive.move_straight(4);
         drive.wait_until_settled();
-        drive.turn_angle(20);
+        drive.turn_angle(10);
         drive.wait_until_settled();
 
         indexer.punch_disk();
-        intake.in();
         pros::delay(2000);
-        intake.stop();
         indexer.punch_disk();
         pros::delay(200);
         flywheel.pause_task();
 
-        drive.turn_angle(-60);
+        drive.turn_angle(90);
         drive.wait_until_settled();
-        drive.move_straight(60);
+        drive.move_straight(-200);
         intake.in();
         flywheel.resume_task();
         drive.wait_until_settled();
@@ -86,7 +98,6 @@ void autonomous() {
         drive.wait_until_settled();
         indexer.punch_disk();
         indexer.punch_disk();
-
         break;
     case none:
         break;
